0bb102d52e154f5ed6fbb1317c347d795473d5f5
Muskan Swarup <muskanswarup680@gmail.com>
2024-06-24 21:52:58 +0530

    completed projects component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,184 @@
+import React from "react";
+import memoryGame from "../assets/memoryGame.png";
+import sundownStudio from "../assets/sundownStudio.png";
+import todoList from "../assets/toDoList.png";
+import foodApp from "../assets/foodApp.png";
+
+function Projects() {
+  return (
+    <div
+      name="projects"
+      className="w-full md:h-screen text-gray-300 bg-[#0a192f] p-8"
+    >
+      <div className="max-w-[1000px] mx-auto flex flex-col p-4 justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Projects
+          </p>
+          <p className="py-6">Check out some of my recent work.</p>
+        </div>
+
+        {/* Container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Grid items */}
+          <div
+            style={{ backgroundImage: `url(${todoList})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-lg flex justify-center items-center mx-auto  content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-black tracking-wider">
+                React Js Application
+              </span>
+
+              <div className="pt-8 text-center">
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid items */}
+          <div
+            style={{ backgroundImage: `url(${memoryGame})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-lg flex justify-center items-center mx-auto  content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-black tracking-wider">
+                React Js Application
+              </span>
+
+              <div className="pt-8 text-center">
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid items */}
+          <div
+            style={{ backgroundImage: `url(${memoryGame})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-lg flex justify-center items-center mx-auto  content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-black tracking-wider">
+                React Js Application
+              </span>
+
+              <div className="pt-8 text-center">
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid items */}
+          <div
+            style={{ backgroundImage: `url(${sundownStudio})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-lg flex justify-center items-center mx-auto  content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-black tracking-wider">
+                React Js Application
+              </span>
+
+              <div className="pt-8 text-center">
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid items */}
+          <div
+            style={{ backgroundImage: `url(${memoryGame})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-lg flex justify-center items-center mx-auto  content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-black tracking-wider">
+                React Js Application
+              </span>
+
+              <div className="pt-8 text-center">
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid items */}
+          <div
+            style={{ backgroundImage: `url(${memoryGame})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-lg flex justify-center items-center mx-auto  content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-black tracking-wider">
+                React Js Application
+              </span>
+
+              <div className="pt-8 text-center">
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
